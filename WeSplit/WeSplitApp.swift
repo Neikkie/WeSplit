1d993af2241e1bfca303e804f21b9e14f0271c6b
Neikkie <neikkie21@outlook.com>
2025-11-08 06:38:32 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Shanique Beckford on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
